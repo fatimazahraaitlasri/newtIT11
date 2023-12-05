15946a485012fbe8651d59c58c96e7f8f91d9301
fatima zahra ait lasri <fatimazahraaitlasri96@gmail.com>
2023-12-05 17:59:13 +0100

    first commit

--- src/Form/Post1Type.php
+++ src/Form/Post1Type.php
@@ -2,29 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class Post1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-          
-            ->add('plainpassword',TextType::class , ['mapped'=>false
-            ])
-            ->add('isVerified')
+            ->add('title')
+            ->add('content')
+            ->add('user')
+            ->add('category')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Post::class,
         ]);
     }
 }
